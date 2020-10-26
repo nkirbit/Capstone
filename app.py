5ffb3f76a9ef718d9304ad408d8e8a491789aef4
Nick Kirby <nickkirby317@gmail.com>
2020-10-26 13:59:18 -0400

    Demoing app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-## This is where my app will go
+from flask import Flask
+app_lulu = Flask(__name__)
+
+@app_lulu.route('/')
+def home_page():
+    # this is a comment, just like in Python
+    # note that the function name and the route argument
+    # do not need to be the same.
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app_lulu.run(debug=True)
