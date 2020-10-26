5e7d1a92152e1c34dd2efcb0a234e6285c34b4f4
Nick Kirby <nickkirby317@gmail.com>
2020-10-26 14:26:20 -0400

    Adding ability to plot simple graph

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask
-app_lulu = Flask(__name__)
+import altair as alt
+import numpy as np
+import pandas as pd
+import json
+from flask import Flask, render_template, request
 
-@app_lulu.route('/')
-def home_page():
-    # this is a comment, just like in Python
-    # note that the function name and the route argument
-    # do not need to be the same.
-    return 'Hello World!'
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app_lulu.run(debug=True)
+def generate_chart(filename):
+
+    df_long = pd.read_csv(f'{filename}.csv')
+    state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", \
+    "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", \
+    "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", \
+    "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan",\
+    "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina",\
+    "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", \
+    "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",\
+    "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",\
+    "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+    base = alt.Chart(df_long).mark_line().encode(
+    alt.X('Date:T'),
+    alt.Y('Cases', title=" "),
+    color = "State:N"
+    )
+
+    # A dropdown filter
+    columns=state_names[0:19]
+    column_dropdown = alt.binding_select(options=columns)
+    column_select = alt.selection_single(
+        fields=['State'],
+        on='doubleclick',
+        clear=False,
+        bind=column_dropdown,
+        name="y ",
+        init={'State': "Alabama"}
+        )
+
+    filter_columns = base.add_selection(
+            column_select
+            ).transform_filter(
+            column_select
+            )
+
+    chart = filter_columns.to_json()
+    return chart
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    chart_json = generate_chart('altair')
+
+    return render_template('index.html', chart_json=chart_json)
+
+if __name__=='__main__':
+    app.run()
