811b8a5c559d4a92d81cc8ac90db81c05eeb0d84
Nick Kirby <nickkirby317@gmail.com>
2020-11-25 08:33:20 -0500

    Adding summary table

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect
 import datetime
 import build_model
 import get_latest_covid
+import model_analysis
 
 app = Flask(__name__)
 app.vars={}
@@ -19,13 +20,23 @@ def index():
 
 @app.route('/make_chart', methods=['GET', 'POST'])
 def make_chart():
-    model = build_model.generate_model(app.vars['business'])
+    business, state = app.vars['business'], app.vars['state']
+    model = build_model.generate_model(business)
     coefs = model.params[1:5]
     average = sum(coefs)/4
+    table_values, f_p_value, output_string = model_analysis.do_some_analysis(model,state,business)
     altair_json, latest_date = get_latest_covid.make_covid_plot(app.vars['state'])
     return render_template('make_chart.html',state_html =app.vars['state'], \
     business_html = app.vars['business'], average=average, \
-    chart_json = altair_json, date_string = latest_date)
+    chart_json = altair_json, date_string = latest_date, \
+    t1 = round(table_values[0][0],3), t2 = round(table_values[1][0],3),\
+    t3 = round(table_values[2][0],3), t4 = round(table_values[3][0],3), \
+    t5 = round(table_values[0][1],3), t6 = round(table_values[1][1],3), \
+    t7 = round(table_values[2][1],3), t8 = round(table_values[3][1],3), \
+    t9 = round(table_values[0][2],3), t10 = round(table_values[1][2],3), \
+    t11 = round(table_values[2][2],3), t12 = round(table_values[3][2],3), \
+    t13 = round(table_values[0][3],3), t14 = round(table_values[1][3],3), \
+    t15 = round(table_values[2][3],3), t16 = round(table_values[3][3],3))
 
 if __name__=='__main__':
     app.run()
